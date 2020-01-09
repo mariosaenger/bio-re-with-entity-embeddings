a695bccd2c3b60df408674abb45156d20828d05d
Mario Sänger <saengema@informatik.hu-berlin.de>
2020-01-09 11:06:11 +0100

    Add module structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name="BioReWithEntityEmbeddings",
+   version="1.0",
+   description="Implementation of large-scale biomedical relation extraction with entity and pair embeddings",
+   license="MIT",
+   author="Mario Sänger",
+   author_email="saengema@informatik.hu-berlin.de",
+   packages=["BioReWithEntityEmbeddings"],  #same as name
+   scripts=[]
+)
